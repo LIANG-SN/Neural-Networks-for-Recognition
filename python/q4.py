935090a2b48ab9fe34a37e1be6a84d9e2f64a932
LIANG-SN <liangsn2001@outlook.com>
2021-04-25 16:07:47 +0800

    text extraction

--- python/q4.py
+++ python/q4.py
@@ -8,6 +8,12 @@ import skimage.filters
 import skimage.morphology
 import skimage.segmentation
 
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+
+
 # takes a color image
 # returns a list of bounding boxes and black_and_white image
 def findLetters(image):
@@ -16,5 +22,30 @@ def findLetters(image):
     # insert processing in here
     # one idea estimate noise -> denoise -> greyscale -> threshold -> morphology -> label -> skip small boxes 
     # this can be 10 to 15 lines of code using skimage functions
+
+    sigma_est = skimage.restoration.estimate_sigma(image, multichannel=True, average_sigmas=True)
+    image = skimage.restoration.denoise_bilateral(image, sigma_color=sigma_est, multichannel=True)
+
+    image = skimage.color.rgb2gray(image)
+    thresh = skimage.filters.threshold_otsu(image)
+    image = image > thresh
+    # Dilation enlarges bright regions and shrinks dark regions.
+    image = skimage.morphology.dilation(image, skimage.morphology.square(2))
+    # Erosion shrinks bright regions and enlarges dark regions.
+    image = skimage.morphology.erosion(image, skimage.morphology.square(8))
+    label_image = skimage.measure.label(image, background=1)
     
+    n = 0
+    average_area = 0
+    for region in skimage.measure.regionprops(label_image):
+        average_area += region.area
+        n+=1
+    average_area /= n
+
+    for region in skimage.measure.regionprops(label_image):
+        # take regions with large enough areas
+        if region.area >= average_area / 5:
+            bboxes.append(region.bbox)
+
+    bw = image
     return bboxes, bw
--- python/run_q4.py
+++ python/run_q4.py
@@ -22,25 +22,88 @@ warnings.simplefilter(action='ignore', category=UserWarning)
 
 for img in os.listdir('../images'):
     im1 = skimage.img_as_float(skimage.io.imread(os.path.join('../images',img)))
-    bboxes, bw = findLetters(im1)
 
-    plt.imshow(bw)
+    bboxes, bw = None, None
+    if True:
+        bboxes, bw = findLetters(im1)
+        # np.savez(img+'.npz', bboxes=bboxes, bw=bw)
+    else:
+        data = np.load(img+'.npz')
+        bboxes = data['bboxes']
+        bw = data['bw']
+
+    if True:
+        plt.imshow(bw, cmap='gray')
+        for bbox in bboxes:
+            minr, minc, maxr, maxc = bbox
+            rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                                    fill=False, edgecolor='red', linewidth=2)
+            plt.gca().add_patch(rect)
+        plt.show()
+
+    # find the rows using..RANSAC, counting, clustering, etc.
+    # cluster the minr of the bounding boxses
+    rows = []
+    row = []
+    i = 1
+    row_average = 0
     for bbox in bboxes:
         minr, minc, maxr, maxc = bbox
-        rect = matplotlib.patches.Rectangle((minc, minr), maxc - minc, maxr - minr,
-                                fill=False, edgecolor='red', linewidth=2)
-        plt.gca().add_patch(rect)
-    plt.show()
-    # find the rows using..RANSAC, counting, clustering, etc.
-    
+        if row_average == 0:
+            row_average = minr
+            row.append(bbox)
+        elif minr < row_average + 125:
+            row.append(bbox)
+            row_average = (row_average * i + minr) / (i + 1)
+            i += 1
+        else:
+            rows.append(row)
+            row_average = minr
+            row = []
+            row.append(bbox)
+            i = 1
+    rows.append(row)
+
     # crop the bounding boxes
     # note.. before you flatten, transpose the image (that's how the dataset is!)
     # consider doing a square crop, and even using np.pad() to get your images looking more like the dataset
-    
+
+    crops = []
+    for row in rows:
+        crops_row = []
+        row.sort(key=lambda x: x[1])
+        for bbox in row:
+            minr, minc, maxr, maxc = bbox
+            pad = (maxr - minr) // 4
+            pattern = np.ones((maxr-minr+pad*2, maxc-minc+pad*2))
+            pattern[pad:-pad, pad:-pad] = bw[minr:maxr, minc:maxc]
+            
+            pattern = skimage.transform.resize(pattern, (32, 32), anti_aliasing=True,\
+                anti_aliasing_sigma=4)
+            # plt.imshow(pattern, cmap='gray')
+            # plt.show()
+            # exit()
+            pattern = (pattern.T).flatten()
+            crops_row.append(pattern)
+        crops.append(crops_row)
     # load the weights
     # run the crops through your neural network and print them out
-    import pickle
-    import string
-    letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
-    params = pickle.load(open('q3_weights.pickle','rb'))
+    if True:
+        import pickle
+        import string
+        letters = np.array([_ for _ in string.ascii_uppercase[:26]] + [str(_) for _ in range(10)])
+        params = pickle.load(open('q3_weights.pickle','rb'))
+        print(img)
+        print('------------')
+        for crops_row in crops:
+            line = ''
+            patterns = np.array(crops_row)
+            h1 = forward(patterns, params, 'layer1')
+            probs = forward(h1, params, 'output', softmax)
+            predicts = np.argmax(probs, axis=1)
+            print(''.join(letters[predicts]))
+        
+        print('------------')
+        # exit()
+
     
